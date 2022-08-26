b26fe3d8c0970be59f151325aee7be51d0c21ecd
stianst <stianst@gmail.com>
2022-08-26 13:40:23 +0200

    Added workflow status section

--- report.sh
+++ report.sh
@@ -10,6 +10,8 @@ PR_EXPIRED_COUNT=`gh api -X GET search/issues -f q="repo:keycloak/keycloak is:pr
 
 echo "# Keycloak Community Dashboard"
 echo ""
+
+echo ""
 echo "## Warnings"
 echo ""
 
@@ -20,6 +22,18 @@ fi
 if [ "$PR_EXPIRED_COUNT" -gt "0" ]; then
   echo "* Some PRs have been around for too long"
 fi
+
+echo ""
+echo "## GitHub Workflows"
+echo ""
+echo "* Nightly release: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak-rel/keycloak-rel/Release%20Nightly)"
+echo "* Keycloak CI: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/Keycloak%20CI)"
+echo "* Keycloak Operator CI: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/Keycloak%20Operator%20CI)"
+echo "* CodeQL JS Adapter: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/CodeQL%20JS%20Adapter)"
+echo "* CodeQL Java: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/CodeQL%20Java)"
+echo "* CodeQL Themes: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/CodeQL%20Themes)"
+echo "* Snyk: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/Snyk)"
+echo "* Trivy: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/Trivy)"
 
 echo ""
 echo "## PRs"
