49663ed31b6b090236e82060b44de5e208955adc
stianst <stianst@gmail.com>
2022-08-26 14:00:26 +0200

    Add bugs

--- report.sh
+++ report.sh
@@ -8,6 +8,9 @@ DATE_EXPIRED=`date -d "-$MAX_AGE month" +%Y-%m-%d`
 PR_COUNT=`gh api -X GET search/issues -f q="repo:keycloak/keycloak is:pr is:open" -f per_page=1 -q .total_count`
 PR_EXPIRED_COUNT=`gh api -X GET search/issues -f q="repo:keycloak/keycloak is:pr is:open created:<$DATE_EXPIRED" -f per_page=1 -q .total_count`
 
+BUG_COUNT=`gh api -X GET search/issues -f q="repo:keycloak/keycloak is:issue is:open label:kind/bug -label:status/triage" -f per_page=1 -q .total_count`
+BUG_TRIAGE_COUNT=`gh api -X GET search/issues -f q="repo:keycloak/keycloak is:issue is:open label:kind/bug label:status/triage" -f per_page=1 -q .total_count`
+
 echo "# Keycloak Community Dashboard"
 echo ""
 
@@ -42,8 +45,9 @@ echo "* [Total PRs](https://github.com/keycloak/keycloak/pulls): $PR_COUNT"
 echo ""
 echo "* [Older than $MAX_AGE months](https://github.com/keycloak/keycloak/pulls?q=created%3A<$DATE_EXPIRED): $PR_EXPIRED_COUNT"
 
-#echo ""
-#echo "PRs more than one year old:"
-
-#gh pr -R keycloak/keycloak list --search "created:<`date -d "-12 month" +%Y-%m-%d`" -L 2 --json number,author,url,createdAt,updatedAt,title
-#gh pr -R keycloak/keycloak list --search "created:<`date -d "-12 month" +%Y-%m-%d`" -L 100
+echo ""
+echo "## Bugs"
+echo ""
+echo "* Open bugs: $BUG_COUNT"
+echo "* Non-triaged bugs: $BUG_TRIAGE_COUNT"
+echo ""
