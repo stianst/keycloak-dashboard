84b8d3d8a3e54b215ff320a461b7ec1b494c76bb
stianst <stianst@gmail.com>
2022-09-02 17:53:20 +0200

    Try different layout

--- report.sh
+++ report.sh
@@ -20,50 +20,42 @@ PR_OPENED_LAST_7=`gh api -X GET search/issues -f q="repo:keycloak/keycloak is:pr
 BUG_COUNT=`gh api -X GET search/issues -f q="repo:keycloak/keycloak is:issue is:open label:kind/bug -label:status/triage" -f per_page=1 -q .total_count`
 BUG_TRIAGE_COUNT=`gh api -X GET search/issues -f q="repo:keycloak/keycloak is:issue is:open label:kind/bug label:status/triage" -f per_page=1 -q .total_count`
 
-echo "## Warnings"
-echo ""
-
+echo "|Warnings|"
+echo "|--------|"
 if [ "$PR_COUNT" -ge "$MAX_PRS" ]; then
-  echo "* Too many open PRs";
+  echo "|Too many open PRs|";
 fi
-
 if [ "$PR_EXPIRED_12_COUNT" -gt "0" ]; then
-  echo "* Some PRs have been around for too long"
+  echo "|Some PRs have been around for too long|"
 fi
 echo ""
 
-echo "## GitHub Workflows"
-echo ""
-echo "* [Nightly release: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak-rel/keycloak-rel/Release%20Nightly)](https://github.com/keycloak-rel/keycloak-rel/actions/workflows/release-nightly.yml)"
-echo ""
-echo "* [Keycloak CI: ![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/keycloak/keycloak/Keycloak%20CI?event=schedule&branch=main)](https://github.com/keycloak/keycloak/actions/workflows/ci.yml?query=event%3Aschedule+branch%3Amain)"
-echo "* [Keycloak Operator CI: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/Keycloak%20Operator%20CI?event=schedule&branch=main)](https://github.com/keycloak/keycloak/actions/workflows/operator-ci.yml?query=event%3Aschedule+branch%3Amain)"
-echo "* [Keycloak QuickStarts CI: ![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/keycloak/keycloak-quickstarts/Quickstarts%20tests?event=schedule)](https://github.com/keycloak/keycloak-quickstarts/actions?query=event%3Aschedule)"
-echo ""
-echo "* [CodeQL JS Adapter: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/CodeQL%20JS%20Adapter?event=schedule&branch=main)](https://github.com/keycloak/keycloak/actions/workflows/codeql-js-adapter-analysis.yml?query=event%3Aschedule+branch%3Amain)"
-echo "* [CodeQL Java: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/CodeQL%20Java?event=schedule&branch=main)](https://github.com/keycloak/keycloak/actions/workflows/codeql-java-analysis.yml?query=event%3Aschedule+branch%3Amain)"
-echo "* [CodeQL Themes: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/CodeQL%20Themes?event=schedule&branch=main)](https://github.com/keycloak/keycloak/actions/workflows/codeql-theme-analysis.yml?query=event%3Aschedule+branch%3Amain)"
-echo ""
-echo "* [Snyk: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/Snyk?event=schedule&branch=main)](https://github.com/keycloak/keycloak/actions/workflows/snyk.yml?query=event%3Aschedule+branch%3Amain)"
-echo "* [Trivy: ![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/Trivy?event=schedule&branch=main)](https://github.com/keycloak/keycloak/actions/workflows/trivy-analysis.yml?query=event%3Aschedule+branch%3Amain)"
+echo "|GitHub Workflows| |"
+echo "|----------------| |"
+echo "|[Nightly release](https://github.com/keycloak-rel/keycloak-rel/actions/workflows/release-nightly.yml)|![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak-rel/keycloak-rel/Release%20Nightly)|"
+echo "|[Keycloak CI](https://github.com/keycloak/keycloak/actions/workflows/ci.yml?query=event%3Aschedule+branch%3Amain)|![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/keycloak/keycloak/Keycloak%20CI?event=schedule&branch=main)|"
+echo "|[Keycloak Operator CI](https://github.com/keycloak/keycloak/actions/workflows/operator-ci.yml?query=event%3Aschedule+branch%3Amain)|![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/Keycloak%20Operator%20CI?event=schedule&branch=main)|"
+echo "|[Keycloak QuickStarts CI](https://github.com/keycloak/keycloak-quickstarts/actions?query=event%3Aschedule)|![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/keycloak/keycloak-quickstarts/Quickstarts%20tests?event=schedule)|"
+echo "|[CodeQL JS Adapter](https://github.com/keycloak/keycloak/actions/workflows/codeql-js-adapter-analysis.yml?query=event%3Aschedule+branch%3Amain)|![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/CodeQL%20JS%20Adapter?event=schedule&branch=main)|"
+echo "|[CodeQL Java](https://github.com/keycloak/keycloak/actions/workflows/codeql-java-analysis.yml?query=event%3Aschedule+branch%3Amain)|![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/CodeQL%20Java?event=schedule&branch=main)|"
+echo "|[CodeQL Themes](https://github.com/keycloak/keycloak/actions/workflows/codeql-theme-analysis.yml?query=event%3Aschedule+branch%3Amain)|![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/CodeQL%20Themes?event=schedule&branch=main)|"
+echo "|[Snyk](https://github.com/keycloak/keycloak/actions/workflows/snyk.yml?query=event%3Aschedule+branch%3Amain)|![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/Snyk?event=schedule&branch=main)|"
+echo "|[Trivy](https://github.com/keycloak/keycloak/actions/workflows/trivy-analysis.yml?query=event%3Aschedule+branch%3Amain)|![GitHub Workflow Status](https://img.shields.io/github/workflow/status/keycloak/keycloak/Trivy?event=schedule&branch=main)|"
 echo ""
 
-echo "## PRs"
-echo ""
-echo "* [Open PRs](https://github.com/keycloak/keycloak/pulls): $PR_COUNT"
-echo ""
-echo "* [Priority PRs](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+is%3Aopen+label%3Apriority%2Fimportant%2Cpriority%2Fcritical): $PR_IMPORTANT_COUNT"
-echo ""
-echo "* [Older than 6 months](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+is%3Aopen+created%3A%3C$DATE_6_EXPIRED): $PR_EXPIRED_6_COUNT"
-echo "* [Older than 12 months](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+is%3Aopen+created%3A%3C$DATE_12_EXPIRED): $PR_EXPIRED_12_COUNT"
-echo "* [Older than 18 months](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+is%3Aopen+created%3A%3C$DATE_18_EXPIRED): $PR_EXPIRED_18_COUNT"
-echo ""
-echo "* [Created last 7 days](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+created%3A%3E$DATE_8_DAYS_AGO): $PR_OPENED_LAST_7"
-echo "* [Closed last 7 days](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+is%3Aclosed+closed%3A%3E$DATE_8_DAYS_AGO): $PR_CLOSED_LAST_7"
+echo "|PRs| |"
+echo "|---|-|"
+echo "|[Open PRs](https://github.com/keycloak/keycloak/pulls)|$PR_COUNT|"
+echo "|[Priority PRs](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+is%3Aopen+label%3Apriority%2Fimportant%2Cpriority%2Fcritical)|$PR_IMPORTANT_COUNT|"
+echo "|[Older than 6 months](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+is%3Aopen+created%3A%3C$DATE_6_EXPIRED)|$PR_EXPIRED_6_COUNT|"
+echo "|[Older than 12 months](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+is%3Aopen+created%3A%3C$DATE_12_EXPIRED)|$PR_EXPIRED_12_COUNT|"
+echo "|[Older than 18 months](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+is%3Aopen+created%3A%3C$DATE_18_EXPIRED)|$PR_EXPIRED_18_COUNT|"
+echo "|[Created last 7 days](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+created%3A%3E$DATE_8_DAYS_AGO)|$PR_OPENED_LAST_7|"
+echo "|[Closed last 7 days](https://github.com/keycloak/keycloak/pulls?q=is%3Apr+is%3Aclosed+closed%3A%3E$DATE_8_DAYS_AGO)|$PR_CLOSED_LAST_7|"
 echo ""
 
-echo "## Bugs"
-echo ""
-echo "* [Open bugs](https://github.com/keycloak/keycloak/issues?q=is%3Aissue+is%3Aopen+label%3Akind%2Fbug+-label%3Astatus%2Ftriage+): $BUG_COUNT"
-echo "* [Non-triaged bugs](https://github.com/keycloak/keycloak/issues?q=is%3Aissue+is%3Aopen+label%3Akind%2Fbug+label%3Astatus%2Ftriage): $BUG_TRIAGE_COUNT"
+echo "|Bugs| |"
+echo "|----| |"
+echo "|[Open bugs](https://github.com/keycloak/keycloak/issues?q=is%3Aissue+is%3Aopen+label%3Akind%2Fbug+-label%3Astatus%2Ftriage+)|$BUG_COUNT|"
+echo "|[Non-triaged bugs](https://github.com/keycloak/keycloak/issues?q=is%3Aissue+is%3Aopen+label%3Akind%2Fbug+label%3Astatus%2Ftriage)|$BUG_TRIAGE_COUNT|"
 echo ""
