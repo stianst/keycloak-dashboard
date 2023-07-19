3a59ad75914004ae183f05eb8eb81893879021d8
stianst <stianst@gmail.com>
2023-07-19 12:22:34 +0200

    Add created date to retried run attempts

--- load-retried-prs.sh
+++ load-retried-prs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -e
 
-for i in $(gh api -X GET repos/keycloak/keycloak/actions/workflows/ci.yml/runs -f per_page=100 -f status=success -f event=pull_request | jq -r '.workflow_runs[] | [.id, .run_attempt] | @csv'); do
+for i in $(gh api -X GET repos/keycloak/keycloak/actions/workflows/ci.yml/runs -f per_page=100 -f status=success -f event=pull_request | jq -r '.workflow_runs[] | [.id, .run_attempt, .created_at] | @csv'); do
     ID=$(echo $i | cut -d ',' -f 1)
     RUN_ATTEMPTS=$(echo $i | cut -d ',' -f 2)
+    CREATED_AT=$(echo $i | cut -d ',' -f 3)
     
     if [ "$RUN_ATTEMPTS" -gt 1 ]; then
         RUN_ATTEMPT=1
         while [ $RUN_ATTEMPT -lt $RUN_ATTEMPTS ]; do
             CONCLUSION=$(gh run view -R keycloak/keycloak $ID --attempt $RUN_ATTEMPT --json conclusion | jq -r .conclusion)
             if [ "$CONCLUSION" == "failure" ]; then
-                echo "https://github.com/keycloak/keycloak/actions/runs/$ID/attempts/$RUN_ATTEMPT"
+                echo "$CREATED_AT,https://github.com/keycloak/keycloak/actions/runs/$ID/attempts/$RUN_ATTEMPT"
             fi
             RUN_ATTEMPT=$(( $RUN_ATTEMPT + 1 )) 
         done
